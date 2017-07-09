96f399c98489d666ec4a1784d90f0adad950dfe0
ikneb <ikneblis@gmail.com>
2017-07-09 23:34:09 +0300

    corect humburger

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,14 @@
-$(function() {
-	/*var mmenu = $("#my-menu").mmenu({"slidingSubmenus": false,
-	                 "extensions": [
-	                        "effect-slide",
-	                        "effect-zoom-menu",
-	                       "pageshadow"
-	               ]}).data('mmenu');*/
-});
-
+var click = false;
 $(document).ready(function () {
-
-	
+		$('.hamburger').click(function() {
+			
+			console.log($("#myPanel").attr("class") == "is-active" );
+			if(click){
+				$(this).removeClass('is-active').removeClass('js-hamburger');
+				click = false;
+			} else {
+				$(this).addClass('is-active').addClass('js-hamburger');
+				click = true;
+			}
+		}); 
 });
