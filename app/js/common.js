c08fd9c271501df2459d386f51ccf7cf1016a99d
ikneb <ikneblis@gmail.com>
2017-07-10 13:32:51 +0300

    menu animate

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,19 @@
 var click = false;
 $(document).ready(function () {
 		$('.hamburger').click(function() {
-			
-			console.log($("#myPanel").attr("class") == "is-active" );
 			if(click){
 				$(this).removeClass('is-active').removeClass('js-hamburger');
+				$('#my-menu').removeClass('show-menu').addClass('close-menu');
+					$('.link-menu').removeClass('in-left-menu').addClass('in-right-menu');
+					$('#hamburger').removeClass('in-left').addClass('in-right');
 				click = false;
 			} else {
 				$(this).addClass('is-active').addClass('js-hamburger');
+				$('#my-menu').removeClass('close-menu').addClass('show-menu');
+				
+					$('.link-menu').addClass('in-left-menu').removeClass('in-right-menu');
+					$('#hamburger').addClass('in-left').removeClass('in-right');
+			
 				click = true;
 			}
 		}); 
